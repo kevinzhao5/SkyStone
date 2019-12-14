09c5508617a28b2f5db1118d78659a1c0bb161cf
Kevin Zhao <kevinzhao.basis@gmail.com>
2019-12-14 08:29:36 -0800

    edts

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBuildStationBlue.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBuildStationBlue.java
@@ -74,6 +74,7 @@ public class AutonomousBuildStationBlue extends LinearOpMode {
         waitForStart();
 
 
+
     }
 
     /**
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBuildStationRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBuildStationRed.java
@@ -74,6 +74,7 @@ public class AutonomousBuildStationRed extends LinearOpMode {
         waitForStart();
 
 
+
     }
 
     /**
