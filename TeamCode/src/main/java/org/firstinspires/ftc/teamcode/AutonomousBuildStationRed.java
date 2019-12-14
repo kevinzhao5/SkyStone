19a83e88c2b94190117d331ccbe09c26582d1ba6
Kevin Zhao <kevinzhao.basis@gmail.com>
2019-12-14 08:28:45 -0800

    edit auton

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBuildStationRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/AutonomousBuildStationRed.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @Autonomous(name="AutonomousBuildStationRed", group="Autonomous")
@@ -15,9 +16,13 @@ public class AutonomousBuildStationRed extends LinearOpMode {
     DcMotor leftFront; //port 0
     DcMotor rightBack; //port 2
     DcMotor rightFront; //port 1
+    DcMotor rnpUp1; //port 1
+    DcMotor rnpUp2; //port 2
 
-    DcMotor rnpUp1;
-    DcMotor rnpUp2;
+    //Servos
+    Servo intakeLeft; //port 1
+    Servo intakeRight; //port 0
+    Servo extension; //port 2
 
     /**
      *
@@ -30,16 +35,19 @@ public class AutonomousBuildStationRed extends LinearOpMode {
         leftFront = hardwareMap.get(DcMotor.class, "leftFront");
         rightBack = hardwareMap.get(DcMotor.class, "rightBack");
         rightFront = hardwareMap.get(DcMotor.class, "rightFront");
-
         rnpUp1 = hardwareMap.get(DcMotor.class, "rnpUp1");
         rnpUp2 = hardwareMap.get(DcMotor.class, "rnpUp2");
+
+        //Initialize Servos
+        intakeLeft = hardwareMap.get(Servo.class, "intakeLeft");
+        intakeRight = hardwareMap.get(Servo.class, "intakeRight");
+        extension = hardwareMap.get(Servo.class, "extension");
 
         //Set zero power behavior
         leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-
         rnpUp1.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rnpUp2.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
 
@@ -48,9 +56,13 @@ public class AutonomousBuildStationRed extends LinearOpMode {
         leftFront.setDirection(DcMotor.Direction.FORWARD);
         rightBack.setDirection(DcMotor.Direction.FORWARD);
         rightFront.setDirection(DcMotor.Direction.FORWARD);
-
         rnpUp1.setDirection(DcMotor.Direction.REVERSE);
         rnpUp2.setDirection(DcMotor.Direction.FORWARD);
+
+        //Set direction of the Servos
+        intakeLeft.setDirection(Servo.Direction.REVERSE);
+        intakeRight.setDirection(Servo.Direction.FORWARD);
+        extension.setDirection(Servo.Direction.REVERSE);
 
         //Set run mode
         rnpUp1.setMode(DcMotor.RunMode.RUN_TO_POSITION);
@@ -60,6 +72,8 @@ public class AutonomousBuildStationRed extends LinearOpMode {
         telemetry.addData("Status", "Initialized");
 
         waitForStart();
+
+
     }
 
     /**
@@ -72,9 +86,5 @@ public class AutonomousBuildStationRed extends LinearOpMode {
         leftBack.setPower(power);
         rightBack.setPower(power);
     }
-
-    /**
-     *
-     */
 
 }
