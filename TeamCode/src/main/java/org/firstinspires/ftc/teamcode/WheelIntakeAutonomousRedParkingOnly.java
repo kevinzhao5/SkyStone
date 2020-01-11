1ca683b91bde3a9c7d323de2bd7f1eae5fb22e03
Kevin Zhao <kevinzhao.basis@gmail.com>
2020-01-10 19:15:17 -0800

    Create WheelIntakeAutonomousRedParkingOnly.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WheelIntakeAutonomousRedParkingOnly.java
@@ -0,0 +1,113 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+
+@Autonomous(name="WheelIntakeAutonomousRedParkingOnly", group="Autonomous")
+public class WheelIntakeAutonomousRedParkingOnly extends LinearOpMode {
+
+    //Objects
+    ElapsedTime runtime = new ElapsedTime();
+
+    //Motors
+    DcMotor leftBack; //port 3
+    DcMotor leftFront; //port 0
+    DcMotor rightBack; //port 2
+    DcMotor rightFront; //port 1
+
+    DcMotor leftWheel; //port 1
+    DcMotor rightWheel; //port 2
+
+    //Sensors
+    ColorSensor color; //port 12c
+
+    @Override
+    public void runOpMode() {
+
+        //Initialize DcMotors
+        leftBack = hardwareMap.get(DcMotor.class, "leftBack");
+        leftFront = hardwareMap.get(DcMotor.class, "leftFront");
+        rightBack = hardwareMap.get(DcMotor.class, "rightBack");
+        rightFront = hardwareMap.get(DcMotor.class, "rightFront");
+
+        leftWheel = hardwareMap.get(DcMotor.class, "leftWheel");
+        rightWheel = hardwareMap.get(DcMotor.class, "rightWheel");
+
+        //Set zero power behavior
+        leftBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightBack.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFront.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        leftWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightWheel.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
+        //Set directions of the motors
+        leftBack.setDirection(DcMotor.Direction.FORWARD);
+        leftFront.setDirection(DcMotor.Direction.FORWARD);
+        rightBack.setDirection(DcMotor.Direction.FORWARD);
+        rightFront.setDirection(DcMotor.Direction.FORWARD);
+
+        leftWheel.setDirection(DcMotor.Direction.FORWARD);
+        rightWheel.setDirection(DcMotor.Direction.REVERSE);
+
+        //Initialize color sensor
+        color = hardwareMap.get(ColorSensor.class, "color");
+
+        //Tell user that initialization is complete
+        telemetry.addData("Status", "Initialized");
+
+        telemetry.addData("red", color.red());
+        telemetry.addData("green", color.green());
+        telemetry.addData("blue", color.blue());
+
+        waitForStart();
+
+        long initTime = System.nanoTime();
+
+        while ((color.red() < 200 || (color.red() > 200 && color.blue() > 200 && color.green() > 200)) && initTime + 3000000000l > System.nanoTime()) {
+            telemetry.addData("color", color.red());
+        }
+
+        pause(0.2);
+
+        setAllDriveMotorPower(0);
+
+    }
+
+    public void setAllDriveMotorPower(double power) {
+        leftFront.setPower(power);
+        rightFront.setPower(power);
+        leftBack.setPower(power);
+        rightBack.setPower(power);
+    }
+
+    public void drive(double x, double y) {
+
+        //Normalize the values if the sum is greater than one to fit motor power
+        double sum = Math.abs(x) + Math.abs(y);
+
+        if (sum > 1) {
+            double newx = x / sum, newy = y / sum;
+            x = newx;
+            y = newy;
+        }
+
+        //Driving
+        leftFront.setPower(-x + y);
+        rightFront.setPower(-x - y);
+        leftBack.setPower(x + y);
+        rightBack.setPower(x - y);
+    }
+
+    public void pause(double s) {
+        long initTime = System.nanoTime();
+        s *= 1000000000;
+        while (System.nanoTime() < initTime + s) {
+
+        }
+    }
+
+}
